a23b24d1ae5577373bea9520c3172aca14f4d8c7
codemaster05330 <thomasj0830@gmail.com>
2020-09-30 02:03:43 +0300

    Fixed hero & menu height

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -10,6 +10,6 @@
 
 {#if Nav[0]}
 <div id="logo" class="abs"><a class="cell tw" href="/" hreflang="en">TA</a></div>
-<nav id="menu" class="tc tw up tc block" class:hidden={!active} class:grid={active}><ul>{#each Nav as [path, name, lang]}<li><a href={$url(path)} class:active={$isActive(path)} on:click={ () => active = !active} hreflang={lang}>{name}</a></li>{/each}</ul></nav>
+<nav id="menu" class="tc tw up tc block" class:hidden={!active} class:grid={active}><ul class="abs cell">{#each Nav as [path, name, lang]}<li><a href={$url(path)} class:active={$isActive(path)} on:click={ () => active = !active} hreflang={lang}>{name}</a></li>{/each}</ul></nav>
 <button id="openMenu" class="abs toggle" class:open={active} class:bgw={!active} on:click={ () => active = !active} title="Open/close main navigation">{#if !active}<svg viewBox="0 0 100 80" width="30" height="30"><title>Open or close navigation</title><desc>Pressing the button in the mobile version opens the main navigation menu. Pressing again closes the menu.</desc><rect width="100" height="20"></rect><rect y="30" width="100" height="20"></rect><rect y="60" width="100" height="20"></rect></svg>{:else}<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 3.752l-4.423-3.752-7.771 9.039-7.647-9.008-4.159 4.278c2.285 2.885 5.284 5.903 8.362 8.708l-8.165 9.447 1.343 1.487c1.978-1.335 5.981-4.373 10.205-7.958 4.304 3.67 8.306 6.663 10.229 8.006l1.449-1.278-8.254-9.724c3.287-2.973 6.584-6.354 8.831-9.245z"/></svg>{/if}</button>
 {/if}
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -2,80 +2,18 @@
 	import Social from "../components/Social.svelte"
 	import { Skills } from "../pages/_data.js"
 </script>
-<style>
-#hello {
-	width: 100%;
-	height: 100vh;
-	overflow: hidden;
-	background-size: cover
-}
-.overlay {
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100vw;
-	height: 100vh;
-	background-color: rgba(0 0 0 / 40%);
-	z-index:1
-}
-#itsMe {
-	position: relative;
-	z-index: 100;
-	max-width: 540px;
-	width: 80%
-}
-#itsMe img {
-	border-radius: 50%;
-	border: 5px solid rgba(255 255 255 / 80%)
-}
-#itsMe picture { margin-bottom: 1.5rem }
-#itsMe h1 {
-	font-size: clamp(2.2rem,2.6rem,10vw);
-	margin-bottom: 10px
-}
-#itsMe p { font-size: 1.2rem }
-#follow { margin-top: 1rem }
-@media screen and (max-width:450px){
-	#hello { background-image: url("/images/railway-360.jpg") }
-}
-@media screen and (min-width:450px) and (max-width:768px){
-	#hello { background-image: url("/images/railway-768.jpg") }
-}
-@media screen and (max-width:750px){
-	#itsMe { padding-bottom: 5rem }
-}
-@media screen and (min-width:768px) and (max-width:1440px){
-	#hello { background-image: url("/images/railway-1440.jpg") }
-}
-@media screen and (min-width:1440px){
-	#hello { background-image: url("/images/railway-2048.jpg") }
-}
-@media screen and (min-width:600px){
-	#itsMe picture,
-	#itsMe img {
-		width: 200px;
-		height: 200px
-	}
-}
-@media screen and (max-width:600px){
-	#itsMe picture,
-	#itsMe img {
-		width: 150px;
-		height: 150px
-	}
-}
-</style>
 
 <div id="hello" class="tc obj cell">
-	<div class="overlay"></div>
-	<div id="itsMe" class="mxa">
-		<picture id="pic" class="mxa">
-			<source srcset="/images/timo.webp" type="image/webp">
-			<source srcset="/images/timo.jpg" type="image/jpeg">
-			<img src="/images/timo.jpg" alt="Timo Anttila">
-		</picture>
-		<h1>Timo Anttila</h1>
-		<p>Full Stack Web Developer, photographer and sea adventurer from Nokia, Finland. Making websites for a living and amuse people as a hobby.</p>
+	<div class="abs cell">
+		<div id="itsMe" class="mxa">
+			<picture id="pic" class="mxa">
+				<source srcset="/images/timo.webp" type="image/webp">
+				<source srcset="/images/timo.jpg" type="image/jpeg">
+				<img src="/images/timo.jpg" alt="Timo Anttila">
+			</picture>
+			<h1>Timo Anttila</h1>
+			<p>Full Stack Web Developer, photographer and sea adventurer from Nokia, Finland. Making websites for a living and amuse people as a hobby.</p>
+		</div>
 	</div>
 </div>
 
