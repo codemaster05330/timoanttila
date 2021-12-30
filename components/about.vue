ebd4ce63bcc2ad1c419d725c958f7b128cbf4993
codemaster05330 <thomasj0830@gmail.com>
2021-12-30 14:39:45 +0200

    Image for about

--- components/about.vue
+++ components/about.vue
@@ -1,6 +1,16 @@
 <template>
 	<div id="about" class="fixedLayout">
-		<MyPic />
+		<picture id="myPic" class="block">
+			<source srcset="/images/timoanttila.webp" type="image/webp" />
+			<source srcset="/images/timoanttila.jpg" type="image/jpeg" />
+			<img
+				class="block w-full h-full object-cover"
+				src="/images/timoanttila.jpg"
+				alt="A self-taught passionate developer Timo Anttila"
+				aria-label="Portrait of Timo Anttila"
+			/>
+		</picture>
+
 		<div v-if="page" id="content" class="text-white grid z-10">
 			<article class="self-center content">
 				<h1 id="bigText" class="z-20 text-yellow text-center m-0">
