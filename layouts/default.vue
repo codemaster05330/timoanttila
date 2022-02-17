613615caa73358f9a138b62b1f5a11d66135823f
codemaster05330 <thomasj0830@gmail.com>
2022-02-17 14:01:41 +0200

    Better accessible links

--- layouts/default.vue
+++ layouts/default.vue
@@ -134,14 +134,13 @@
 					<ul
 						id="accessibilityList"
 						class="m-0 p-0"
-						role="menu"
 						aria-labelledby="accessibilityToggle"
 					>
 						<li
 							v-for="(e, i) in colors"
 							:key="i"
 							class="inline-block align-middle"
-							role="menuitem"
+							role="listitem"
 						>
 							<button
 								@click="fixStyles(e.name)"
@@ -175,7 +174,7 @@
 							</button>
 						</li>
 
-						<li class="inline-block align-middle" role="menuitem">
+						<li class="inline-block align-middle" role="listitem">
 							<button
 								@click="changeSize(fontSize + 1)"
 								class="
@@ -205,7 +204,7 @@
 							</button>
 						</li>
 
-						<li class="inline-block align-middle" role="menuitem">
+						<li class="inline-block align-middle" role="listitem">
 							<button
 								@click="changeSize(fontSize - 1)"
 								class="
@@ -237,137 +236,155 @@
 					</ul>
 				</div>
 
-				<a
-					class="
-						w-14
-						h-14
-						mx-auto
-						inline-block
-						align-middle
-						bg-transparent
-						border-0
-						md:m-0
-						p-0
-						hide-210
-					"
-					href="https://twitter.com/_timoanttila"
-					title="Timo Anttila on Twitter"
-					aria-label="Open the link to Timo Anttila's profile on Twitter"
+				<ul
+					class="p-0 m-0 inline-block align-middle"
+					title="You have right to stalk me"
+					aria-label="List of Timo Anttila's social media channels"
 				>
-					<div class="grid w-full h-full">
-						<svg
-							class="self-center mx-auto"
-							xmlns="http://www.w3.org/2000/svg"
-							width="32"
-							height="32"
-							viewBox="0 0 24 24"
+					<li class="inline-block align-middle" role="none">
+						<a
+							class="
+								w-14
+								h-14
+								mx-auto
+								inline-block
+								align-middle
+								bg-transparent
+								border-0
+								md:m-0
+								p-0
+								hide-210
+							"
+							href="https://twitter.com/_timoanttila"
+							title="Timo Anttila on Twitter"
+							aria-label="Open the link to Timo Anttila's profile on Twitter"
+							role="listitem"
 						>
-							<title>Twitter</title>
-							<path
-								d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
-							/>
-						</svg>
-					</div>
-				</a>
+							<div class="grid w-full h-full">
+								<svg
+									class="self-center mx-auto"
+									xmlns="http://www.w3.org/2000/svg"
+									width="32"
+									height="32"
+									viewBox="0 0 24 24"
+								>
+									<title>Twitter</title>
+									<path
+										d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+									/>
+								</svg>
+							</div>
+						</a>
+					</li>
 
-				<a
-					class="
-						w-14
-						h-14
-						mx-auto
-						inline-block
-						align-middle
-						bg-transparent
-						border-0
-						md:m-0
-						p-0
-						hide-270
-					"
-					href="https://github.com/timoanttila"
-					title="Timo Anttila on Github"
-					aria-label="Open the link to Timo Anttila's profile on Github"
-				>
-					<div class="grid w-full h-full">
-						<svg
-							class="self-center mx-auto"
-							xmlns="http://www.w3.org/2000/svg"
-							width="32"
-							height="32"
-							viewBox="0 0 24 24"
+					<li class="inline-block align-middle" role="none">
+						<a
+							class="
+								w-14
+								h-14
+								mx-auto
+								inline-block
+								align-middle
+								bg-transparent
+								border-0
+								md:m-0
+								p-0
+								hide-270
+							"
+							href="https://github.com/timoanttila"
+							title="Timo Anttila on Github"
+							aria-label="Open the link to Timo Anttila's profile on Github"
+							role="listitem"
 						>
-							<title>Github</title>
-							<path
-								d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-							/>
-						</svg>
-					</div>
-				</a>
+							<div class="grid w-full h-full">
+								<svg
+									class="self-center mx-auto"
+									xmlns="http://www.w3.org/2000/svg"
+									width="32"
+									height="32"
+									viewBox="0 0 24 24"
+								>
+									<title>Github</title>
+									<path
+										d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+									/>
+								</svg>
+							</div>
+						</a>
+					</li>
 
-				<a
-					class="
-						w-14
-						h-14
-						mx-auto
-						inline-block
-						align-middle
-						bg-transparent
-						border-0
-						md:m-0
-						p-0
-						hide-390
-					"
-					href="https://github.com/TuspeDesign"
-					title="Tuspe Design on Github"
-					aria-label="Open the link to Timo Anttila's company's profile on Github"
-				>
-					<div class="grid w-full h-full">
-						<svg
-							class="self-center mx-auto"
-							xmlns="http://www.w3.org/2000/svg"
-							width="32"
-							height="32"
-							viewBox="0 0 24 24"
+					<li class="inline-block align-middle" role="none">
+						<a
+							class="
+								w-14
+								h-14
+								mx-auto
+								inline-block
+								align-middle
+								bg-transparent
+								border-0
+								md:m-0
+								p-0
+								hide-390
+							"
+							href="https://github.com/TuspeDesign"
+							title="Tuspe Design on Github"
+							aria-label="Open the link to Timo Anttila's company's profile on Github"
+							role="listitem"
 						>
-							<title>Github</title>
-							<path
-								d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-							/>
-						</svg>
-					</div>
-				</a>
+							<div class="grid w-full h-full">
+								<svg
+									class="self-center mx-auto"
+									xmlns="http://www.w3.org/2000/svg"
+									width="32"
+									height="32"
+									viewBox="0 0 24 24"
+								>
+									<title>Github</title>
+									<path
+										d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+									/>
+								</svg>
+							</div>
+						</a>
+					</li>
 
-				<a
-					class="
-						w-14
-						h-14
-						mx-auto
-						inline-block
-						align-middle
-						bg-transparent
-						border-0
-						md:m-0
-						p-0
-						hide-330
-					"
-					href="mailto:moro@tuspe.com"
-					title="Send email"
-					aria-label="The link activates your default email application or website"
-				>
-					<div class="grid w-full h-full">
-						<svg
-							class="self-center mx-auto"
-							xmlns="http://www.w3.org/2000/svg"
-							width="32"
-							height="32"
-							viewBox="0 0 24 24"
+					<li class="inline-block align-middle" role="none">
+						<a
+							class="
+								w-14
+								h-14
+								mx-auto
+								inline-block
+								align-middle
+								bg-transparent
+								border-0
+								md:m-0
+								p-0
+								hide-330
+							"
+							href="mailto:moro@tuspe.com"
+							title="Send email"
+							aria-label="The link activates your default email application or website"
+							role="listitem"
 						>
-							<title>Envelope</title>
-							<path
-								d="M12.042 23.648c-7.813 0-12.042-4.876-12.042-11.171 0-6.727 4.762-12.125 13.276-12.125 6.214 0 10.724 4.038 10.724 9.601 0 8.712-10.33 11.012-9.812 6.042-.71 1.108-1.854 2.354-4.053 2.354-2.516 0-4.08-1.842-4.08-4.807 0-4.444 2.921-8.199 6.379-8.199 1.659 0 2.8.876 3.277 2.221l.464-1.632h2.338c-.244.832-2.321 8.527-2.321 8.527-.648 2.666 1.35 2.713 3.122 1.297 3.329-2.58 3.501-9.327-.998-12.141-4.821-2.891-15.795-1.102-15.795 8.693 0 5.611 3.95 9.381 9.829 9.381 3.436 0 5.542-.93 7.295-1.948l1.177 1.698c-1.711.966-4.461 2.209-8.78 2.209zm-2.344-14.305c-.715 1.34-1.177 3.076-1.177 4.424 0 3.61 3.522 3.633 5.252.239.712-1.394 1.171-3.171 1.171-4.529 0-2.917-3.495-3.434-5.246-.134z"
-							/>
-						</svg>
-					</div>
-				</a>
+							<div class="grid w-full h-full">
+								<svg
+									class="self-center mx-auto"
+									xmlns="http://www.w3.org/2000/svg"
+									width="32"
+									height="32"
+									viewBox="0 0 24 24"
+								>
+									<title>Envelope</title>
+									<path
+										d="M12.042 23.648c-7.813 0-12.042-4.876-12.042-11.171 0-6.727 4.762-12.125 13.276-12.125 6.214 0 10.724 4.038 10.724 9.601 0 8.712-10.33 11.012-9.812 6.042-.71 1.108-1.854 2.354-4.053 2.354-2.516 0-4.08-1.842-4.08-4.807 0-4.444 2.921-8.199 6.379-8.199 1.659 0 2.8.876 3.277 2.221l.464-1.632h2.338c-.244.832-2.321 8.527-2.321 8.527-.648 2.666 1.35 2.713 3.122 1.297 3.329-2.58 3.501-9.327-.998-12.141-4.821-2.891-15.795-1.102-15.795 8.693 0 5.611 3.95 9.381 9.829 9.381 3.436 0 5.542-.93 7.295-1.948l1.177 1.698c-1.711.966-4.461 2.209-8.78 2.209zm-2.344-14.305c-.715 1.34-1.177 3.076-1.177 4.424 0 3.61 3.522 3.633 5.252.239.712-1.394 1.171-3.171 1.171-4.529 0-2.917-3.495-3.434-5.246-.134z"
+									/>
+								</svg>
+							</div>
+						</a>
+					</li>
+				</ul>
 			</div>
 		</aside>
 
