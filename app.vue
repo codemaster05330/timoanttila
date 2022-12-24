cd437a01cf6e21ab35fd0b057e5dd37ba9abbe41
codemaster05330 <thomasj0830@gmail.com>
2022-12-24 22:55:20 +0200

    favicon.ico
    Signed-off-by: Timo Anttila <code@tuspe.com>

--- app.vue
+++ app.vue
@@ -21,6 +21,7 @@ useHead({
 		{rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png', href: '/apple-touch-icon.png'},
 		{rel: 'icon', sizes: '32x32', type: 'image/png', href: '/favicon-32x32.png'},
 		{rel: 'icon', sizes: '16x16', type: 'image/png', href: '/favicon-16x16.png'},
+		{rel: 'icon', type: 'image/x-icon', href: url + '/favicon.ico'},
 		{rel: 'manifest', href: '/site.webmanifest'},
 		{rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#422e43'}
 	]
